6942b69b4bd6b673934351c6bdd17b8aa1a47cb7
sladeberryman <slade12berryman@gmail.com>
2023-10-31 20:29:19 -0500

    Add comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
+    // When form is submitted
     $("#surveyForm").submit(function(event) {
       event.preventDefault();
   
       var username = $("#text-input").val();
       var password = $("#password-input").val();
   
+      // Only allow user through if username and pwd is csis
       if (username === "csis" && password === "csis") {
         window.location.href = "survey.html";
       } else {
