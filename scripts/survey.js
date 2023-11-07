054ca2c1eedad79242a0b346fe9b7437baf730ed
sladeberryman <slade12berryman@gmail.com>
2023-11-07 13:27:06 -0600

    Finish India

--- scripts/survey.js
+++ scripts/survey.js
@@ -1,8 +1,38 @@
 $(document).ready(function() {
+
+  $('input[name="radio"]').change(function() {
+    var selectedValue = $(this).val();
+    var options = '';
+
+    // check the first radio input and create options for the second radio input
+    if (selectedValue === 'Transit') {
+      options += '<div class="form-check form-check-inline">';
+      options += '<input class="form-check-input" type="radio" name="radio2" id="radio3" value="With family">';
+      options += '<label class="form-check-label" for="radio3">With family</label>';
+      options += '</div>';
+      options += '<div class="form-check form-check-inline">';
+      options += '<input class="form-check-input" type="radio" name="radio2" id="radio4" value="On my own/with friends">';
+      options += '<label class="form-check-label" for="radio4">On my own/with friends</label>';
+      options += '</div>';
+    } else if (selectedValue === 'On Campus') {
+      options += '<div class="form-check form-check-inline">';
+      options += '<input class="form-check-input" type="radio" name="radio2" id="radio5" value="Dormitory">';
+      options += '<label class="form-check-label" for="radio5">Dormitory</label>';
+      options += '</div>';
+      options += '<div class="form-check form-check-inline">';
+      options += '<input class="form-check-input" type="radio" name="radio2" id="radio6" value="Apartment">';
+      options += '<label class="form-check-label" for="radio6">Apartment</label>';
+      options += '</div>';
+    }
+
+    // Update the options of the second radio input
+    $('#living-situation-part-b').html('<label>Living Situation Part B:</label><br>' + options);
+  });
+
     $('form').submit(function(event) {
       event.preventDefault(); // Prevent the form from submitting default
   
-      // Get the values entered by the user
+      // get the values entered by the user
       var email = $('#email-input').val();
       var favoriteClass = $('#dropdown').val();
       var mood = $('#text-input').val();
@@ -10,7 +40,7 @@ $(document).ready(function() {
       var livingSituation = $('input[name="radio"]:checked').val();
       var customizationOptions = [];
   
-      // Special loop to fetch checkboxes
+      // loop to fetch checkboxes
       $('input[name="checkbox"]:checked').each(function() {
         customizationOptions.push($(this).val());
       });
@@ -30,4 +60,4 @@ $(document).ready(function() {
       // Redirect to the survey results page
       window.location.href = 'survey_results.html';
     });
-  });
+});
--- scripts/survey_results.js
+++ scripts/survey_results.js
@@ -20,4 +20,34 @@ $(document).ready(function() {
   
     // Clear the stored data from localStorage
     localStorage.clear();
+
+    /////////////////// code below is for populating from favs.json
+    // use fetch to grab data
+    fetch('lib/favs.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      // Access the data from the JSON file
+      var favoriteClass = data.class;
+      var mood = data.mood;
+      var rant = data.rant;
+      var livingSituation = data.living;
+      var customizationOptions = data.options;
+      var birthday = data.birthday;
+      
+      // Display the data in the results container
+      var resultsContainer = $('#favs-container');
+      resultsContainer.append('<p><strong>Favorite Class</strong>: ' + favoriteClass + '</p>');
+      resultsContainer.append('<p><strong>Mood</strong>: ' + mood + '</p>');
+      resultsContainer.append('<p><strong>Rant</strong>: ' + rant + '</p>');
+      resultsContainer.append('<p><strong>Living Situation</strong>: ' + livingSituation + '</p>');
+      resultsContainer.append('<p><strong>Parking Options</strong>: ' + customizationOptions + '</p>');
+      resultsContainer.append('<p><strong>Birthday</strong>: ' + birthday + '</p>');
+    })
+    // log any errors for fun times
+    .catch(function(error) {
+      console.log('Error:', error);
+    });
+
   });
